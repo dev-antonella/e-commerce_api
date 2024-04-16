517d650795298f917aefd028ca02e16bdaab942f
Namzug <guzman.deleonalonso@gmail.com>
2024-04-16 18:23:58 -0300

    Server port
    Se corrige variable de entorno utilizada

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-const port = process.env.DB_PORT;
+const port = process.env.APP_PORT;
 
 app.use(express.json());
 app.use(routes);
 
 app.listen(port, () =>
-  console.log(`Server running on http://${process.env.APP_DOMAIN}.\n`)
+    console.log(`Server running on http://${process.env.APP_DOMAIN}.\n`)
 );
 
 // TODO: escribir las variables de entorno.
