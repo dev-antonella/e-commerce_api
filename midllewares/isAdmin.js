0206640e91ee21655f02c137d479c7efbfc46a0f
Logan Aresche <logan.aresche@anima.edu.uy>
2024-04-23 20:11:44 -0300

    Update isAdmin.js

--- midllewares/isAdmin.js
+++ midllewares/isAdmin.js
@@ -1,14 +1,16 @@
-const isAdmin = async (req, res, next) =>{ {
- { if(req.auth.roll = String(user))
+const isAdmin = async (req, res, next) => {
+  try {
+    const userRole = req.auth.roll;
+
+    if (userRole === "admin") {
       return res
         .status(403)
-        .json({ message: "You must have admin permissions." });
-    }{
-  else
-    next();
-}
+        .json({ message: "You must have administrator permissions." });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error("Error in middleware:", error);
+    return res.status(500).json({ message: "Internal server error." });
   }
 };
-
-
-
